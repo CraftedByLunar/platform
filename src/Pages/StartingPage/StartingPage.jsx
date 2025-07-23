ecae96ed0ae7fa91ec5f4df5f3e2ff446bcb1c08
MohitTiwariBytes <mohittiwaridev@gmail.com>
2025-07-23 09:46:35 +0530

    Saving some work (not an actuall commit i just wanna save some work)

--- src/Pages/StartingPage/StartingPage.jsx
+++ src/Pages/StartingPage/StartingPage.jsx
@@ -1,25 +1,12 @@
 import "./StartingPage.css";
 import React, { useState } from "react";
-import MDConverter from "../../Components/MDConverter/MDConverter";
 import { useEffect } from "react";
 
 export default function StartingPage() {
-    const [markdownContent, setMarkdownContent] = useState(``);
-
-    useEffect(() => {
-        fetch('/Introduction.md')
-            .then(response => response.text())
-            .then(text => {
-                setMarkdownContent(text);
-            })
-            .catch(error => console.error("Error loading markdown file:", error));
-    }, []);
-
-
     return (
         <div className="main-start-page">
             <div className="start-page">
-                <MDConverter markdown={markdownContent}></MDConverter>
+                {/* code this shit dude more like the clerk.dev start page */}
             </div>
         </div>
     );
--- src/Pages/TestingSpace/TestingSpace.jsx
+++ src/Pages/TestingSpace/TestingSpace.jsx
@@ -11,10 +11,8 @@ import MageneticText from "../../Components/MagneticText"
 import BouncyButton from "../../Components/BouncyButton"
 import ImageTrail from "../../Components/ImageTrail"
 
-
 export default function TestingSpace() {
     const images = ["https://cdn.pixabay.com/photo/2018/04/14/23/21/cute-3320456_960_720.jpg", "https://cdn.pixabay.com/photo/2021/11/05/19/01/cappadocia-6771879_1280.jpg", "https://cdn.pixabay.com/photo/2023/12/13/22/29/young-woman-8447841_1280.jpg", "https://cdn.pixabay.com/photo/2022/05/05/12/57/flowers-7176104_640.jpg", "https://cdn.pixabay.com/photo/2022/10/22/17/00/gull-7539615_1280.jpg"]
-
 
     return (
         <div className="main-testing-space">
